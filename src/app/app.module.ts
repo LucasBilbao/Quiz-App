19ac1d7094f44d9055b224e8d3efc900158e79a4
LucasBilbao <lucasbm789@gmail.com>
2022-03-29 12:00:35 +0400

    feat: add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { QuizModule } from './quiz/quiz.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, QuizModule],
+  imports: [BrowserModule, QuizModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
