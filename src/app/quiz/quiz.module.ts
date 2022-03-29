bec847c55b79287d1cfac62e2e3c5760b78c1ba3
LucasBilbao <lucasbm789@gmail.com>
2022-03-29 15:44:23 +0400

    feat: add quiz module

--- src/app/quiz/quiz.module.ts
+++ src/app/quiz/quiz.module.ts
@@ -2,8 +2,37 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreModule } from '../core/core.module';
 
+// Components
+import { QuizPageComponent } from './components/quiz-page/quiz-page.component';
+import { StartGameComponent } from './components/start-game/start-game.component';
+import { AuthorizationComponent } from './components/authorization/authorization.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+// Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTreeModule } from '@angular/material/tree';
+
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, CoreModule  ],
+  declarations: [
+    QuizPageComponent,
+    StartGameComponent,
+    AuthorizationComponent,
+    NavBarComponent,
+  ],
+  imports: [
+    CommonModule,
+    CoreModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatIconModule,
+    MatTreeModule,
+  ],
+  exports: [QuizPageComponent],
 })
 export class QuizModule {}
