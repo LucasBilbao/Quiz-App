40a0ebcfeb167bca0a86ad34973f9fbeea52b46a
LucasBilbao <lucasbm789@gmail.com>
2022-03-29 16:02:02 +0400

    feat: add core module
    Add core module and implement some module import/export.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../App-Routing/app-routing.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FormsModule],
-  exports: [FormsModule],
+  imports: [CommonModule, FormsModule, AppRoutingModule],
+  exports: [FormsModule, AppRoutingModule],
 })
 export class CoreModule {}
