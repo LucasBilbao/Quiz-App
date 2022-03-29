b40382dfc0c87e62eeaee6e70b891ca33c3f282a
LucasBilbao <lucasbm789@gmail.com>
2022-03-29 18:01:22 +0400

    feat: implement some filler paths for routing

--- src/app/App-Routing/app-routing.module.ts
+++ src/app/App-Routing/app-routing.module.ts
@@ -3,6 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { QuizPageComponent } from '../quiz/components/quiz-page/quiz-page.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: QuizPageComponent },
+  { path: 'sign-up', component: QuizPageComponent },
+  { path: 'quiz', component: QuizPageComponent },
+  { path: 'create-question', component: QuizPageComponent },
+  { path: 'history', component: QuizPageComponent },
 ];
 
 @NgModule({
