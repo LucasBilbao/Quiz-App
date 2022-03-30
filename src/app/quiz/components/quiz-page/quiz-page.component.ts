828290bb48fd217b91ba5812bd3d3f9c7ebb7435
LucasBilbao <lucasbm789@gmail.com>
2022-03-30 15:47:53 +0400

    feat: implement text formatting

--- src/app/quiz/components/quiz-page/quiz-page.component.ts
+++ src/app/quiz/components/quiz-page/quiz-page.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { QuizService } from '../../services/quiz/quiz.service';
+import { Question } from '../../models/question.model';
 
 @Component({
   selector: 'app-quiz-page',
   templateUrl: './quiz-page.component.html',
-  styleUrls: ['./quiz-page.component.scss']
+  styleUrls: ['./quiz-page.component.scss'],
 })
 export class QuizPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
