956e08609a9691d072904f4ebad0166e4ea39bf0
LucasBilbao <lucasbm789@gmail.com>
2022-03-30 19:09:23 +0400

    feat: implement a more specific routing

--- src/app/App-Routing/app-routing.module.ts
+++ src/app/App-Routing/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionsComponent } from '../quiz/components/quiz-page/questions/questions.component';
 import { QuizPageComponent } from '../quiz/components/quiz-page/quiz-page.component';
+import { StartQuizComponent } from '../quiz/components/quiz-page/start-quiz/start-quiz.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
-  { path: 'sign-in', component: QuizPageComponent },
-  { path: 'sign-up', component: QuizPageComponent },
-  { path: 'quiz', component: QuizPageComponent },
-  { path: 'create-question', component: QuizPageComponent },
-  { path: 'history', component: QuizPageComponent },
+  { path: 'sign-in', component: StartQuizComponent },
+  { path: 'sign-up', component: StartQuizComponent },
+  { path: 'quiz', component: StartQuizComponent },
+  { path: 'quiz/questions', component: QuestionsComponent },
+  { path: 'create-question', component: StartQuizComponent },
+  { path: 'history', component: StartQuizComponent },
 ];
 
 @NgModule({
