511fb71cfd8ab415d6fcc613d43d6ec9b09f31c2
LucasBilbao <lucasbm789@gmail.com>
2022-03-30 14:58:12 +0400

    feat: add questions component
    Add questions component and implement some basic code.

---
+++ src/app/quiz/components/questions/questions.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Question } from '../../models/question.model';
+import { QuizService } from '../../services/quiz/quiz.service';
+
+@Component({
+  selector: 'app-questions',
+  templateUrl: './questions.component.html',
+  styleUrls: ['./questions.component.scss'],
+})
+export class QuestionsComponent implements OnInit {
+  questions: Question[] = [];
+
+  isLoading: boolean = true;
+
+  constructor(private quizServices: QuizService) {}
+
+  ngOnInit(): void {
+    this.quizServices.fetchQuestions().then((questions) => {
+      this.questions = questions;
+      this.isLoading = false;
+    });
+  }
+
+}
