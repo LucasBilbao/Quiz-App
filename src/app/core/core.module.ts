2d3e6c20ee981925569e971f39ea700685f9d8bb
LucasBilbao <lucasbm789@gmail.com>
2022-03-30 15:44:35 +0400

    feat: add new modules in the core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from '../App-Routing/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from '../app-routing/app-routing.module';
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FormsModule, AppRoutingModule],
-  exports: [FormsModule, AppRoutingModule],
+  imports: [CommonModule, FormsModule, AppRoutingModule, HttpClientModule],
+  exports: [FormsModule, AppRoutingModule, HttpClientModule],
 })
 export class CoreModule {}
