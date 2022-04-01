5cb1ecce17fc2bb7ff72be224ddc18ff46fed331
LucasBilbao <lucasbm789@gmail.com>
2022-04-01 17:14:49 +0400

    feat: implement new routing component

--- src/app/App-Routing/app-routing.module.ts
+++ src/app/App-Routing/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateQuestionComponent } from '../quiz/components/quiz-page/create-question/create-question.component';
 import { QuestionsComponent } from '../quiz/components/quiz-page/questions/questions.component';
 import { QuizPageComponent } from '../quiz/components/quiz-page/quiz-page.component';
 import { StartQuizComponent } from '../quiz/components/quiz-page/start-quiz/start-quiz.component';
@@ -10,7 +11,7 @@ const routes: Routes = [
   { path: 'sign-up', component: StartQuizComponent },
   { path: 'quiz', component: StartQuizComponent },
   { path: 'quiz/questions', component: QuestionsComponent },
-  { path: 'create-question', component: StartQuizComponent },
+  { path: 'create-question', component: CreateQuestionComponent },
   { path: 'history', component: StartQuizComponent },
 ];
 
